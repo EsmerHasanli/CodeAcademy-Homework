95d39f2599ea8f73bd67bd0317f3a65cf63f9e35
EsmerHasanli <tu6wu93gq@code.edu.az>
2023-10-13 05:23:20 +0400

    initial commit

---
+++ 12/New folder/main.js
@@ -0,0 +1,37 @@
+// ! task1
+// ! task2
+// ! task3
+
+// ! task4
+let numbers = [1,4,2,6,8,2,1,7,7];
+let count = 0;
+for(let i=0 ; i<numbers.length ; i++) {
+    if(numbers[i] === numbers[i]){
+        count++
+    }
+
+}
+console.log(count)
+
+// ! task5
+// let numbers = [1,4,2,6,8,2,1,7,7];
+// let sum
+// let result
+// for(let i=0; i<numbers.lenght; i++ ){
+//     sum += numbers[i] 
+//     result = sum / numbers.lenght
+// } 
+// console.log(result) //? undefiend
+
+
+// ! task6
+// let names = ["Ali","Vali","Ahmad","Muhammed","Yusif"];
+// for(let i=0 ; i<names.length ; i++) {
+//     if(names[i].length > 4 ){   
+//         console.log(names[i]) 
+//     }
+// }
+
+
+
+// ! task7
